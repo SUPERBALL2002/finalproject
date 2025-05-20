a2423c3977f5ce4d68adb0c0f9192ec422621211
micsmic611 <161224898+micsmic611@users.noreply.github.com>
2025-05-20 21:14:24 +0700

    Update server.js

--- be/server.js
+++ be/server.js
@@ -1,9 +1,13 @@
-const express = require('express');
+ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./db');  // เชื่อมต่อกับฐานข้อมูล
 const userRoutes = require('./routes/users');  
+const lessonRoutes = require('./routes/lessons');
+const subjectRoutes = require('./routes/subjects');
+const examRoutes = require('./routes/exams');
 const authenticateToken = require('./Middleware/JWT');
+//
 
 const app = express();
 app.use(cors());
@@ -13,6 +17,9 @@ const PORT = 3001;
 
 // ใช้ routes จากไฟล์ 'routes/users.js'
 app.use('/api/users', userRoutes);
+app.use('/api/lessons', lessonRoutes);
+app.use('/api/subjects', subjectRoutes);
+app.use('/api/exams', examRoutes);
 app.get('/api/protected', authenticateToken, (req, res) => {
     res.status(200).json({ message: 'Protected route accessed', user: req.user });
   });
@@ -20,3 +27,4 @@ app.get('/api/protected', authenticateToken, (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+// ประกาศใน routes 
